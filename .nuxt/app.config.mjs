6e38e59c780407c00c59beb97a22a2c6e65c6a3b
Bhavesh Patel <bhavesh.patel200@gmail.com>
2022-11-16 00:37:33 +0530

    Initial commit

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from 'D:/Projects/plugins/vue3-vite/vue3-datatable-document/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default defuFn(inlineConfig)
