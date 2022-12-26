1e46853cac89bdfa4e558b552846f9d347044a8c
Bhavesh Patel <bhavesh.patel200@gmail.com>
2022-12-26 16:07:22 +0530

    added new demos

--- pages/advanced.vue
+++ pages/advanced.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <div class="flex items-center justify-between mb-5">
-            <h2 class="text-3xl">Basic</h2>
-            <a target="_blank" href="https://github.com/bhaveshpatel200/vue3-datatable-document/blob/main/pages/basic.vue" class="btn">
+            <h2 class="text-3xl">Advanced</h2>
+            <a target="_blank" href="https://github.com/bhaveshpatel200/vue3-datatable-document/blob/main/pages/advanced.vue" class="btn">
                 <icon-github class="w-5 h-5 mr-2" />
                 View Source
             </a>
