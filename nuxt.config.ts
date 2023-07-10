fdc97893beb42bf8cf3fbea7185592c7994c1671
Bhavesh Patel <bhavesh.patel200@gmail.com>
2023-07-10 22:40:02 +0530

    cors issue user api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,42 +1,48 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  app: {
-    head: {
-      title: "Vue3-Datatable",
-      htmlAttrs: {
-        lang: "en",
-      },
-      meta: [
-        { charset: "utf-8" },
-        {
-          name: "viewport",
-          content:
-            "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
+    app: {
+        head: {
+            title: 'Vue3-Datatable',
+            htmlAttrs: {
+                lang: 'en',
+            },
+            meta: [
+                { charset: 'utf-8' },
+                {
+                    name: 'viewport',
+                    content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+                },
+                { hid: 'description', name: 'description', content: '' },
+                { name: 'format-detection', content: 'telephone=no' },
+            ],
+            link: [
+                // { rel: "icon", type: "image/x-icon", href: "/favicon.png" },
+                {
+                    rel: 'stylesheet',
+                    href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap',
+                },
+            ],
         },
-        { hid: "description", name: "description", content: "" },
-        { name: "format-detection", content: "telephone=no" },
-      ],
-      link: [
-        // { rel: "icon", type: "image/x-icon", href: "/favicon.png" },
-        {
-          rel: "stylesheet",
-          href: "https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap",
+    },
+    nitro: {
+        routeRules: {
+            '/api/**': {
+                cors: true,
+            },
         },
-      ],
     },
-  },
-  css: ["@/assets/css/tailwind.css"],
+    css: ['@/assets/css/tailwind.css'],
 
-  modules: ["@pinia/nuxt"],
+    modules: ['@pinia/nuxt'],
 
-  publicRuntimeConfig: {
-    SITE_URL: process.env.SITE_URL,
-  },
-
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+    publicRuntimeConfig: {
+        SITE_URL: process.env.SITE_URL,
     },
-  },
+
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
 });
